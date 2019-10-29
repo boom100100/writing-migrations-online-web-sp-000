9f6c7d4f2a6493e35a4017bb2a85004229680645
Bernadette Davis <bsd231@nyu.edu>
2019-10-29 18:42:19 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -8,6 +8,12 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
+<<<<<<< HEAD
+=======
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+>>>>>>> 3e4cf63f1f22c618ad2fe2db397fbbdbaff3b816
     end
   end
 end
